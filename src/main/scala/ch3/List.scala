e8fc74d1aae6a7fa957b41ef6fb03af2d1564e25
yuk1ty <yuki.mul.tiplus@gmail.com>
2020-05-29 02:47:38 +0900

    drop parenthess in if clause

--- src/main/scala/ch3/List.scala
+++ src/main/scala/ch3/List.scala
@@ -17,39 +17,39 @@ object List {
     case Cons(x, xs) => x * product(xs)
   }
 
-  def apply[A](as: A*): List[A] = if (as.isEmpty) Nil else Cons(as.head, apply(as.tail: _*))
+  def apply[A](as: A*): List[A] = if as.isEmpty then Nil else Cons(as.head, apply(as.tail: _*))
   
   def setHead[A](xs: List[A], head: A): List[A] = xs match {
     case Nil => Cons(head, Nil)
     case Cons(_, t) => Cons(head, t)
   }
-  
+
   def drop[A](l: List[A], n: Int): List[A] = {
-    if (n <= 0) l
+    if n <= 0 then l
     else l match {
       case Cons(h, t) => drop(t, n - 1)
       case Nil => Nil
     }
   }
-  
+
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
     case Cons(h, t) if f(h) => dropWhile(t, f)
     case _ => l
-  } 
-  
+  }
+
   def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
     case Nil => a2
     case Cons(h, t) => Cons(h, append(t, a2))
   }
-  
+
   def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
     case Nil => z
     case Cons(x, xs) => f(x, foldRight(xs, z)(f))
   }
-  
-  def map[A, B](as: List[A])(f: A => B): List[B] = 
+
+  def map[A, B](as: List[A])(f: A => B): List[B] =
     foldRight(as, Nil: List[B])((h, t) => Cons(f(h), t))
-  
-  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = 
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
     foldRight(map(as)(f), Nil: List[B])(append)
 }
