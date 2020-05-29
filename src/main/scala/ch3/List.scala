667eeb30ad771fc134d39158f1bce565c8854dfc
yuk1ty <yuki.mul.tiplus@gmail.com>
2020-05-29 10:11:51 +0900

    Add foldLeft

--- src/main/scala/ch3/List.scala
+++ src/main/scala/ch3/List.scala
@@ -42,6 +42,11 @@ object List {
     case Cons(h, t) => Cons(h, append(t, a2))
   }
 
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
+  
   def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
     case Nil => z
     case Cons(x, xs) => f(x, foldRight(xs, z)(f))
