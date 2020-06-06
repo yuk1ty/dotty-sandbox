a59d420a96818980ac8f0aaef4ca232527b0ed4f
yuk1ty <yuki.mul.tiplus@gmail.com>
2020-06-07 01:36:50 +0900

    Add Applicative

---
+++ src/main/scala/ch12/Applicative.scala
@@ -0,0 +1,11 @@
+package ch12
+
+import ch11.Functor
+
+trait Applicative[F[_]] extends Functor[F] {
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  def unit[A](a: => A): F[A]
+  
+  def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
+    as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
+}
