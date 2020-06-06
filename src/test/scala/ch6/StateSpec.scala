c9140839caabf582e33f1fe46705547f04cadb94
yuk1ty <yuki.mul.tiplus@gmail.com>
2020-06-06 23:59:59 +0900

    Add StateSpec

--- src/test/scala/ch6/StateSpec.scala
+++ src/test/scala/ch6/StateSpec.scala
@@ -1,5 +1,33 @@
 package ch6
 
-class StateSpec {
+import org.scalatest.funsuite.AnyFunSuite
 
+class StateSpec extends AnyFunSuite {
+  test("vending machine state machine") {
+    enum Input {
+      case Coin
+      case Turn
+    }
+    
+    case class Machine(locked: Boolean, candies: Int, coins: Int)
+    
+    def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+      val modification = (i: Input) => (s: Machine) => {
+        (i, s) match {
+          case (_, Machine(_, 0, _)) => s
+          case (Input.Coin, Machine(false, _, _)) => s
+          case (Input.Turn, Machine(true, _, _)) => s
+          case (Input.Coin, Machine(true, candy, coin)) =>
+            Machine(false, candy, coin + 1)
+          case (Input.Turn, Machine(false, candy, coin)) =>
+            Machine(true, candy - 1, coin)
+        }
+      }
+      
+      for {
+        _ <- State.sequence(inputs map (State.modify[Machine] _ compose modification))
+        s <- State.get
+      } yield (s.coins, s.candies)
+    }
+  }
 }
